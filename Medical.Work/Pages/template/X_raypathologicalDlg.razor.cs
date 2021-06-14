79f0d6a988421adee2de275f0b58b292197882c0
452845625@qq.com <452845625@qq.com>
2021-06-14 23:41:33 +0800

    121211212121212

---
+++ Medical.Work/Pages/template/X_raypathologicalDlg.razor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BootstrapBlazor.Components;
+using Microsoft.AspNetCore.Components;
+
+namespace Medical.Work.Pages.template
+{
+    public partial class X_raypathologicalDlg : ComponentBase, IResultDialog
+    {
+        Task IResultDialog.OnClose(DialogResult result)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
